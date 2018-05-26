35160305ea2aeeb17f48121dd2c4469725b962d4
Fiip <gutfilip@gmail.com>
2018-05-26 18:05:27 +0300

    Arduino part

--- gutSemafor.ino
+++ gutSemafor.ino
@@ -53,109 +53,87 @@ void eqK(){
   float r;
   while((r=analogRead(Eqk))!=0){
     for(int i=0;i<5;i++){
-    Serial.println(r);
-    
+    Serial.println("EarthQ ON");
     digitalWrite(BIred,LOW);
-    Serial.println("BIred OFF");
     digitalWrite(BIgreen,LOW);
-    Serial.println("BIgreen OFF");
     digitalWrite(RGBgreen,HIGH);
-    Serial.println("RGBgreen HIGH");
     digitalWrite(RGBred,HIGH);
-    Serial.println("RGBred HIGH");
-    //digitalWrite(RGBblue,HIGH);
     digitalWrite(LED,HIGH);
-    Serial.println("LED HIGH");
     delay(500);
-
     digitalWrite(RGBgreen,LOW);
-    Serial.println("RGBgreen LOW");
     digitalWrite(RGBred,LOW);
-    Serial.println("RGBred LOW");
     digitalWrite(RGBblue,LOW);
-    Serial.println("RGBblue LOW");
     digitalWrite(LED,LOW);
-    Serial.println("LED OFF");
     delay(500);
     }
   }
+   Serial.println("EarthQ OFF");
   }
 
 
 void start(){
     digitalWrite(RGBred,HIGH);
-    Serial.println("RGBred HIGH");
     digitalWrite(BIred,HIGH);
-    Serial.println("BIred HIGH");
     delay(10000);
     digitalWrite(RGBred,LOW);
-    Serial.println("RGBred LOW");
     digitalWrite(BIred,LOW);
-    Serial.println("BIred LOW");
   }
 void cars(){
+   Serial.println("CarS OFF");
   eqK();
   digitalWrite(RGBred,HIGH);
-  Serial.println("RGBred HIGH");
   eqK();
   rightG();
   eqK();
   digitalWrite(LED,LOW); 
-  Serial.println("LED LOW"); 
   //blink green while red 
   eqK();
   digitalWrite(RGBred,LOW);
-  Serial.println("RGBred LOW");
   eqK();
   digitalWrite(RGBgreen,HIGH);
-  Serial.println("RGBgreen HIGH");
   for(int i=0;i<30;i++){
+   Serial.println("Cars ON");
     eqK();
     digitalWrite(RGBgreen,HIGH);
-    Serial.println("RGBgreen HIGH");
     delay(500); 
-    
+    Serial.println("CarS OFF");
   }  
   
   eqK();
   digitalWrite(RGBred,HIGH);
-  Serial.println("RGBred HIGH");
   eqK();
   delay(3000);
   eqK();
   digitalWrite(RGBgreen,LOW);
-  Serial.println("RGBgreen LOW");
   eqK();  
   }
 
 void rightG(){  
+  Serial.print("IntermitentG ON")
   for(int i=0;i<10;i++){
     eqK();
     if(i==1){
       tone(Buzzer,150);
+      Serial.println("Pedestrian ON");
       digitalWrite(BIred,LOW);
-      Serial.println("BIred LOW");
       digitalWrite(BIgreen,HIGH);
-      Serial.println("BIgreen HIGH");
       eqK();
       }
     if(i==9){
       eqK();
       digitalWrite(BIgreen,LOW);
-      Serial.println("BIgreen LOW");
       digitalWrite(BIred,HIGH);
-      Serial.println("BIred HIGH");
       tone(Buzzer,300);
+      Serial.println("Pedestrian OFF");
       }
     digitalWrite(LED,HIGH);
-    Serial.println("LED HIGH");
     delay(500);
     eqK();
     digitalWrite(LED,LOW);
-    Serial.println("LED LOW"); 
     delay(500); 
   }
   eqK();
+   Serial.println("IntermitentG OFF");
 }
 
 
